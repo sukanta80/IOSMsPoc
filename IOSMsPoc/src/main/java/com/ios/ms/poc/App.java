35a32c74f7b50df476843785c844a200266cc1bd
Misra <sukanta.misra@cgi.com>
2017-09-14 17:32:47 +0530

    New Project Structure Created

---
+++ IOSMsPoc/src/main/java/com/ios/ms/poc/App.java
@@ -0,0 +1,13 @@
+package com.ios.ms.poc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
